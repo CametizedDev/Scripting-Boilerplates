7985120bd682b5a0bb71259e6c1871b66405a4d0
Cametized <cameron.reese.f@gmail.com>
2025-09-28 13:28:54 -0400

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,1 +1,7 @@
-local toolbar = print("hgjsjdjfjfj")
+local base = plugin:CreateToolbar("ScriptUtils")
+local triab = plugin:CreateToolbar("TRIAUtils")
+local note = "hey! if you wanna help on this plugin, go to https://github.com/CametizedDev/Scripting-Utilities!
+
+print(note)
+local functionbase = base:CreateButton("Insert Function", "Inserts the base of a function at your cursor.", "rbxassetid://0"
+local triabase = triab:CreateButton("Insert a TRIA GetButtonEvent", "Inserts a button function for TRIA.os", "rbxassetid://0"
