e0513b8d7aad77a64957bb5aa20d497ca9e3cc73
Cametized <cameron.reese.f@gmail.com>
2025-09-29 00:22:07 -0400

    recode

--- Main.lua
+++ Main.lua
@@ -1,28 +1,51 @@
-local base = plugin:CreateToolbar("ScriptUtils")
-local triab = plugin:CreateToolbar("TRIAUtils")
-local note = "hey! if you wanna help on this plugin, go to https://github.com/CametizedDev/Scripting-Utils"
+local toolbar = plugin:CreateToolbar("Boilerplates")
 
-local functionbase = base:CreateButton("Insert Function", "Inserts the base of a function at your cursor.", "")
-local triabase = triab:CreateButton("Insert a TRIA GetButtonEvent", "Inserts a button function for TRIA.os", "")
+local bps = {
+	{
+		name = "LocalScript Plate",
+		description = "Boilerplate for a LocalScript",
+		icon = "rbxassetid://6023426926",
+		template = [[
+		local players = game:GetService("Players")
+		local player = players.LocalPlayer or players.PlayerAdded:Wait()
+		]]
+	},
+	{
+		name = "ModuleScript Plate",
+		description = "Boilerplate for a ModuleScript",
+		icon = "rbxassetid://6023426926",
+		template = [[
+		local module = {}
 
-local function insertFunc()
-	return [[
+		function module.baseModule()
+			print("Hello, world! (From a Module)")
+		end
 
-local function funcName()
-	print("Hello World")
+		return module
+			]]
+	},
+	{
+		name = "Server Script Plate",
+		description = "Boilerplate for a Regular Script",
+		icon = "rbxassetid://6023426926",
+		template = [[
+		print("Script running from: " .. script:GetFullName())
+			]]
+	}
+}
+
+local function insertBP(info)
+	local selec = game:GetService("Selection"):Get()[1]
+
+	scInst = Instance.new(info.class)
+	scInst.Name = info.name
+	scInst.Source = info.code
+	scInst.Parent = selec or game:GetService("ServerScriptService")
 end
 
-]]
+for _, bp in pairs(bps) do
+	local btn = toolbar:CreateButton(bp.name, bp.description, "")
+	btn.Click:Connect(function())
+		insertBP(bp)
+	end)
 end
-
-functionbase.Click:Connect(function()
-	print(note)
-	local ses = game:GetService("ScriptEditorService")
-	local editor = ses:FindScriptDocument(plugin:GetSelectedScript())
-
-	if editor then
-		editor:InsertText(insertFunc())
-	else
-		warn("open script dum")
-	end
-end)
